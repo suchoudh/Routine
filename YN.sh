5dbfcd00737a78250d9309a815ea1b95e2dfebf2
suchoudh <sunil.choudhary@gmail.com>
2020-07-22 21:03:56 +0530

    todays

--- YN.sh
+++ YN.sh
@@ -27,7 +27,7 @@ echo "                                          Editing file $file"
 echo "=================File : $file =========" 
 read -p "Press any key to resume ..."  
 echo $WK_DATE_TIME >> $file 
-vi +$$ "$file" 
+#vi +$$ "$file" 
 done
 clear # clears screen 
 echo Routine home is $RoutineHome 
