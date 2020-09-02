b1fd28322aafafcbb23543a73ae44bd31f6d51bf
suchoudh <sunil.choudhary@gmail.com>
2020-09-02 12:19:47 +0530

    sunrise and sunset added

--- Imp.sh
+++ Imp.sh
@@ -1,31 +1,29 @@
-########WeekNumber is 48#####***QuesAboutYesterday.sh ##################################
+########WeekNumber is 35 in year 2020#####***QuesAboutYesterday.sh ##################################
 ##   File                : QuesAboutYesterday.sh 
 ##   Creation DateTime   :48_20191126-22_52_52
-##   Last Updated Date   :48_20191126-22_52_52 
+##   Last Updated Date   :2020-08-27 Thu 02:50 PM 
 ##   Author              : Sunil Choudhary 
-##   Remarks             :  
+##   Remarks             : List of imp milestones in a day that must be tracked and not missed. 
 ##########111#############################################################
 
 # Setting up env 
-#. ./setupEnv.sh  # Commented and to be removed as resets the Yesterday flag 
+. ./setupEnv.sh  # Commented and to be removed as resets the Yesterday flag 
 echo " This is a log for todays or yesterday " 
 echo " day starts at 0000 hrs and ends at 2400 hrs" 
 touch $NOW1/BoD_ran_on_$WK_DATE_TIME.delme
 
 cd $RoutineHome # Go to root folder and cycle through all log files.  
 
-for file in *.BoD
+for file in Imp*.*
 
 do
 # do something on $file 
 clear 
-# ls -ltr  *.BoD 
-find . -name '*.BoD' -mmin +14 -exec ls -ltrh {} \; # List files older than 14 min 
+ls -ltr Imp*.*    
 echo "                                          Editing file $file" 
 echo "=================File : $file =========" 
 read -p "Press any key to resume ..."
 
-sleep 7 
 
 echo $WK_DATE_TIME >> $file 
 vi +$$ "$file" 
--- README.md
+++ README.md
@@ -14,4 +14,8 @@ and than there is result ...
 So Consolidate your pointers that you note in terms of two levels 
     How was your performance today 
 	What is the result that you are getting. 
+2020-08-27 Thu 03:06 PM
+	need to chop chop work in 20 or 40 min cycles and use that time in pomodoro to get things done in a faster way 
+	Accept that you get up late and it is your disposition. 
+	Use Zettlekasten to do the contacts work This will work like a miracle.
 
